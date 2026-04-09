5211169a89c740570609e9cbf3e3347c3d91ddf1
cristianomarianoufsc-ops <231576260+cristianomarianoufsc-ops@users.noreply.github.com>
2026-04-09 10:32:18 -0400

    Update repository with the game launcher

--- client/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import GameScene from '@/components/GameScene';
-
-export default function Home() {
-  return (
-    <div className="w-full h-screen bg-black">
-      <GameScene />
-    </div>
-  );
-}
